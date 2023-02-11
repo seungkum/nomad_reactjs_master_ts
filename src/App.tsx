cc27f8fcbb85118d8d691fb945cc6f470fad16cc
Minny <min7490@gmail.com>
2023-02-11 19:21:51 +0900

    #3.3 Optional Props (09:48)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Circle from "./Circle";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Circle bgColor='teal' borderColor='tomato' />
+            <Circle bgColor='tomato' text='im here' />
+        </div>
+    );
 }
 
 export default App;
