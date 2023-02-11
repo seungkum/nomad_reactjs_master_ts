902484f2df4e4ee1524e7d1158847b72b323e680
Minny <min7490@gmail.com>
2023-02-11 20:30:43 +0900

    #3.6 Themes (08:42)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import styled from "styled-components";
 import Circle from "./Circle";
 
 function App() {
+    const Container = styled.div`
+        background-color: ${(props) => props.theme.bgColor}; //만약 틀리면 알려준다. 에러라고
+    `;
+    const H1 = styled.h1`
+        color: ${(props) => props.theme.textColor};
+    `;
     return (
         <div className='App'>
-            {/* <Circle bgColor='teal' borderColor='tomato' />
-            <Circle bgColor='tomato' text='im here' /> */}
+            <Container>
+                <H1> protected</H1>
+            </Container>
         </div>
     );
 }
