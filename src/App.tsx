dfa6577380f8bd833f8e45d0d2b4d322eb640b4e
Minny <min7490@gmail.com>
2023-02-11 20:15:02 +0900

    3.5 Forms

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import Circle from "./Circle";
 function App() {
     return (
         <div className='App'>
-            <Circle bgColor='teal' borderColor='tomato' />
-            <Circle bgColor='tomato' text='im here' />
+            {/* <Circle bgColor='teal' borderColor='tomato' />
+            <Circle bgColor='tomato' text='im here' /> */}
         </div>
     );
 }
